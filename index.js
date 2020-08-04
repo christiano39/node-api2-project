58e8df7e2d6f3bd6c19c3c7e193978f31d43b292
christiano39 <carneson39@gmail.com>
2020-08-04 13:51:10 -0600

    added cors

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cors = require('cors');
+
 const postsRoutes = require('./posts/postsRoutes');
 const port = 8000;
 
 const server = express();
 server.use(express.json());
+server.use(cors());
 
 server.use('/api/posts', postsRoutes);
 
