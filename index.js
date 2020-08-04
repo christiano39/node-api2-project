fe98d4fe8926cc402632dbbe6b55e918d425e297
christiano39 <carneson39@gmail.com>
2020-08-03 21:56:04 -0600

    set up boilerplate for express router

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const postsRoutes = require('./posts/postsRoutes');
+const port = 8000;
+
+const server = express();
+server.use(express.json());
+server.use('/api/posts', postsRoutes);
+
+server.listen(port, () => console.log('server running...'));
